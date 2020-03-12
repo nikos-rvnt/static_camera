27d955802d1bf6d17f98d40ba08c87c973e9294e
Nikos_Arv <nikos_arv@bitbucket.org>
2020-03-12 12:39:56 +0200

    tonbCamera, ScanFire

---
+++ ScanFire.py
@@ -0,0 +1,102 @@
+
+import cv2 as cv
+import numpy as np
+import time
+import TonboCamera
+import alarmRequests
+
+
+def StaticScan():
+
+	camPositions = list(range(52,-80,-5)) + list(range(-80,52,5))
+	camPositions = camPositions = [ camPositions[i]%360 for i in range(len(camPositions)) ]
+	camPos_indx = 0
+	# cam_thrm = cv.VideoCapture("rtsp://192.168.2.233:8555/video1")
+	cam_thrm = cv.VideoCapture(0)
+	Tonbo = TonboCamera.Tonbo()
+	
+	NumFramesPerPosition = 300
+	# Scan
+	while 1:
+		
+		Tonbo.setPanPos(camPositions[camPos_indx])
+		camPos_indx = (camPos_indx + 1) % len(camPositions)
+		time.sleep(1)
+		Tonbo.setTiltPos(5)
+		time.sleep(10)
+		
+		for i in range(NumFramesPerPosition):
+			retThrm, frameThrm = cam_thrm.read()
+			check = np.sum(frameThrm[50:500,50:650,:] > 250)
+			
+			if check > 15:
+				# Fire Detected From Thermal Camera
+				Lat, Long = Tonbo.getCoordinates(1, Tonbo.getPanPos())
+				print("Fire.............")
+				# Stop Scanning
+				return True, Lat, Long
+	
+	# if break while loop
+	return False, -1, -1
+
+
+def UAVScan():
+	
+	cam_quad = cv.VideoCapture("rtmp://127.0.0.1:1935/live/quad")
+	cntDetected = 0
+	while 1:
+		
+		retQuad, frmQuad = cam_quad.read()
+		qq = (np.sum((frmQuad[:,:,2].astype(int)-frmQuad[:,:,1].astype(int))>50))
+		if qq:
+			cntDetected +=1
+		
+		if cntDetected > 0:
+			quadCoords = alarmRequests.getQuadCoords()
+			latQuad = float(quadCoords[0])
+			longQuad = float(quadCoords[1])
+			cv.imwrite( '/home/theasis/software/static_cam/fireFrame.jpg', frmQuad)
+			print("Validated...")
+			return True, latQuad, longQuad
+	
+	# if break while loop
+	return False, -1, -1
+
+
+def checkIfDroneIsHome():
+
+	droneHomeCoords = [ 38.123456, 21.567890]
+	#isDroneHome = True
+
+	droneCurrentCoords = alarmRequests.getQuadCoords()
+	droneCurrentCoords[0] = float(droneCurrentCoords[0])
+	droneCurrentCoords[1] = float(droneCurrentCoords[1])
+	if np.abs(droneHomeCoords[0] - droneCurrentCoords[0])<0.00001 and if np.abs(droneHomeCoords[1] - droneCurrentCoords[1])<0.00001:
+		isDroneHome = True
+	else:  
+		isDroneHome = False
+
+	return isDroneHome
+
+
+
+if __name__ == '__main__':
+
+
+
+	while 1:
+
+		FireDetected, Lat, Long = StaticScan()
+
+		if FireDetected and checkIfDroneIsHome():
+
+			alarmRequests.newAlarm( Lat, Long)
+			time.sleep(5)
+			FireValidated, latQuad, longQuad = UAVScan()
+			if FireValidated:
+				alarmRequests.validateAlarm( latQuad, longQuad)
+			else:
+				print("Not Validated")
+			alarmRequests.deleteAlarm()
+
+
---
+++ TonboCamera.py
@@ -0,0 +1,139 @@
+import math
+import socket
+import time
+#import threading
+#from multiprocessing import Process
+
+UDP_IP = "192.168.2.233"
+UDP_PORT = 5004
+
+class Tonbo:
+
+	def __init__( self, Zoom = 4, Brightness = 0, Gain = 0, initTheta  = 141,
+				Latitude =  38.101849, Longitude =  21.345674):
+		# setZoom(Zoom)
+		# setBrightness(Brightness)
+		# setGain(Gain)
+		self.initTheta = initTheta
+		self.Latitude = Latitude
+		self.Longitude = Longitude
+
+	def sendToCam( self, message1):
+		message1.append((sum(message1)+1)%256)
+		encodedMessage1 = bytes(message1)
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		sock.sendto(encodedMessage1, (UDP_IP, UDP_PORT))
+
+	def getZoom(self):
+		import binascii        
+		message2 = [255, 1, 0, 0x61, 0, 0, 0x62]
+		encodedMessage2 = bytes(message2)
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		sock.sendto(encodedMessage2, (UDP_IP, UDP_PORT))
+		a = sock.recv(7)
+		b = binascii.hexlify(a)
+		#print(b)
+		Zoom = 0.01*(int(int(b,16)/pow(16,2))%pow(16,4))
+		return Zoom
+
+	# def setBrightness( self, brightness):
+	# 	self.sendToCam([255,1,0,0x4b, msb, lsb])
+
+	# def getBrightness(self):
+	# 	import binascii        
+	# 	message2 = [255, 1, 0, 0x61, 0, 0, 0x62]
+	# 	encodedMessage2 = bytes(message2)
+	# 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	# 	sock.sendto(encodedMessage2, (UDP_IP, UDP_PORT))
+	# 	a = sock.recv(7)
+	# 	b = binascii.hexlify(a)
+	# 	#print(b)
+	# 	Zoom = 0.01*(int(int(b,16)/pow(16,2))%pow(16,4))
+	# 	return Zoom
+
+	# def setGain( self, gain):
+	# 	self.sendToCam([255,1,0,0x4b, msb, lsb])
+
+	# def getGain(self):
+	# 	import binascii        
+	# 	message2 = [255, 1, 0, 0x61, 0, 0, 0x62]
+	# 	encodedMessage2 = bytes(message2)
+	# 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	# 	sock.sendto(encodedMessage2, (UDP_IP, UDP_PORT))
+	# 	a = sock.recv(7)
+	# 	b = binascii.hexlify(a)
+	# 	#print(b)
+	# 	Zoom = 0.01*(int(int(b,16)/pow(16,2))%pow(16,4))
+	# 	return Zoom
+	
+	def setZoom( self, comval):
+		lsb = int((comval*100)%256)
+		msb = int((comval*100)/256)
+		self.sendToCam([255,1,0,0x5F, msb, lsb])
+
+	def setPanPos( self, ang):
+		lsb = int((ang*100)%256)
+		msb = int((ang*100)/256)
+		self.sendToCam([255,1,0,0x4b, msb, lsb])
+		
+	def setTiltPos( self, ang):
+		lsb = int((ang*100)%256)
+		msb = int((ang*100)/256)
+		self.sendToCam([0xFF,0x0,0x0,0x4d, msb, lsb])
+
+	def setZeroPos(self):
+		self.sendToCam([0xFF,0x1,0x0,0x49, 0x0, 0x0])
+
+	def StopRotation(self):
+		self.sendToCam([255, 1, 0, 0, 0, 0])
+
+	def MoveLeft( self, speed):
+		self.sendToCam([255, 1, 0, 4, speed, 0])
+
+	def MoveRight( self, speed):
+		self.sendToCam([255, 1, 0, 2, speed, 0])
+
+	def getPanPos(self):
+		import binascii
+		message2= [255, 1, 0, 0x51, 0, 0, 0x52]
+		encodedMessage2 = bytes(message2)
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		sock.sendto(encodedMessage2, (UDP_IP, UDP_PORT))
+		a = sock.recv(7)
+		b = binascii.hexlify(a)
+		#print(b)
+		Pan = 0.01*(int(int(b,16)/pow(16,2))%pow(16,4))
+		#print(Pan)
+		return Pan
+
+	def getTiltPos(self):
+		import binascii
+		message2= [255, 1, 0, 0x53, 0, 0, 0x54]
+		encodedMessage2 = bytes(message2)
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		sock.sendto(encodedMessage2, (UDP_IP, UDP_PORT))
+		a = sock.recv(7)
+		b = binascii.hexlify(a)
+		Tilt = 0.01*(int(int(b,16)/pow(16,2))%pow(16,4))
+		return Tilt
+
+	def stopAllAction(self):
+		self.sendToCam([255,1,0,0, 0, 0])
+
+	def getCoordinates(self, D, Pan):
+		import numpy as np
+		import math
+		r_earth = 6378
+		R = Pan - self.initTheta
+		dx = D * np.cos(math.radians(R))
+		dy = D * np.sin(math.radians(R))
+
+		new_latitude  = self.Latitude  + (dx / r_earth) * (180 / np.pi)
+		new_longitude = self.Longitude - (dy / r_earth) * (180 / np.pi) / np.cos(self.Latitude * np.pi/180);
+		return new_latitude, new_longitude
+
+	def imCoords2Angles( self, x, y):
+		a1 = math.atand((x - 315.8608),3.5989)*180/pi
+		a2 = math.atand((x - 451.5614),4.2493)*180/pi
+		return(a1,a2)
+
